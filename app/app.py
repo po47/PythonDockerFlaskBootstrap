ac22b9434fd8dd18e56b64fc2e5359a0ee04b68b
po47 <po47@njit.edu>
2020-05-15 16:53:08 -0400

    fix: index

--- app/app.py
+++ app/app.py
@@ -49,7 +49,7 @@ def form_update_post(homestat_id):
                  request.form.get('Sq_ft'), request.form.get('Rooms'), request.form.get('Baths'),
                  request.form.get('Age_of_House'), homestat_id)
     sql_update_query = """UPDATE homestats t SET t.Street= %s, t.City = %s, t.Zip_Code = %s, t.Selling_Price = %s, t.Listing_Price= %s, 
-    t.Sqt_ft = %s, t.Rooms = %s, t.Baths = %s, t.Age_f_House = %s WHERE t.id = %s """
+    t.Sqt_ft = %s, t.Rooms = %s, t.Baths = %s, t.Age_of_House = %s WHERE t.id = %s """
     cursor.execute(sql_update_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
@@ -67,7 +67,8 @@ def form_insert_post():
                  request.form.get('Selling_Price'), request.form.get('Listing_Price'),
                  request.form.get('Sq_ft'), request.form.get('Rooms'), request.form.get('Baths'),
                  request.form.get('Age_of_House'))
-    sql_insert_query = """INSERT INTO homestats (Street,City,Zip_Code,Selling_Price,Listing_Price,Sq_ft, Rooms, Baths, Age_of_House) VALUES (%s, %s,%s, %s,%s, %s,%s) """
+    sql_insert_query = """INSERT INTO homestats (Street,City,Zip_Code,Selling_Price,Listing_Price,Sq_ft, Rooms, 
+    Baths, Age_of_House) VALUES (%s, %s,%s, %s,%s, %s,%s) """
     cursor.execute(sql_insert_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
@@ -110,6 +111,15 @@ def api_add() -> str:
 
 @app.route('/api/v1/homestats/<int:homestat_id>', methods=['PUT'])
 def api_edit(homestat_id) -> str:
+    cursor = mysql.get_db().cursor()
+    content = request.json
+    inputData = (
+        content['Street'], content['City'], content['Zip_Code'], content['Selling_Price'], content['Listing_Price'],
+        content['Sq_ft'], content['Rooms'], content['Baths'], content['Age_of_House'], homestat_id)
+    sql_update_query = """UPDATE homestats t SET t.Street= %s, t.City = %s, t.Zip_Code = %s, t.Selling_Price = %s, t.Listing_Price= %s, 
+    t.Sqt_ft = %s, t.Rooms = %s, t.Baths = %s, t.Age_of_House = %s WHERE t.id = %s """
+    cursor.execute(sql_update_query, inputData)
+    mysql.get_db().commit()
     resp = Response(status=201, mimetype='application/json')
     return resp
 

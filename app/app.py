bc39f4e8a845a1eb875cd950f7ffcf2618b021d6
po47 <po47@njit.edu>
2020-05-16 00:08:46 -0400

    fix: changed app

--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+from typing import List, Dict
 import simplejson as json
 from flask import Flask, request, Response, redirect
 from flask import render_template
@@ -11,111 +12,105 @@ app.config['MYSQL_DATABASE_HOST'] = 'db'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_PORT'] = 3306
-app.config['MYSQL_DATABASE_DB'] = 'homeData'
+app.config['MYSQL_DATABASE_DB'] = 'bioData'
 mysql.init_app(app)
 
 
 @app.route('/', methods=['GET'])
 def index():
-    user = {'username': 'Home Stats'}
+    user = {'username': 'Bio Stats'}
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM homestats')
+    cursor.execute('SELECT * FROM biostats')
     result = cursor.fetchall()
-    return render_template('index.html', title='Home', user=user, homestats=result)
+    return render_template('index.html', title='Home', user=user, biostats=result)
 
 
-@app.route('/view/<int:homestat_id>', methods=['GET'])
-def record_view(homestat_id):
+@app.route('/view/<int:biostat_id>', methods=['GET'])
+def record_view(biostat_id):
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM homestats WHERE id=%s', homestat_id)
+    cursor.execute('SELECT * FROM biostats WHERE id=%s', biostat_id)
     result = cursor.fetchall()
-    return render_template('view.html', title='View Form', homestat=result[0])
+    return render_template('view.html', title='View Form', biostat=result[0])
 
 
-@app.route('/edit/<int:homestat_id>', methods=['GET'])
-def form_edit_get(homestat_id):
+@app.route('/edit/<int:biostat_id>', methods=['GET'])
+def form_edit_get(biostat_id):
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM homestats WHERE id=%s', homestat_id)
+    cursor.execute('SELECT * FROM biostats WHERE id=%s', biostat_id)
     result = cursor.fetchall()
-    return render_template('edit.html', title='Edit Form', homestat=result[0])
+    return render_template('edit.html', title='Edit Form', biostat=result[0])
 
 
-@app.route('/edit/<int:homestat_id>', methods=['POST'])
-def form_update_post(homestat_id):
+@app.route('/edit/<int:biostat_id>', methods=['POST'])
+def form_update_post(biostat_id):
     cursor = mysql.get_db().cursor()
-    inputData = (request.form.get('Street'), request.form.get('City'), request.form.get('Zip_Code'),
-                 request.form.get('Selling_Price'), request.form.get('Listing_Price'), request.form.get('Sq_ft'),
-                 request.form.get('Rooms'),
-                 request.form.get('Baths'), request.form.get('Age_of_House'), homestat_id)
-    sql_update_query = """UPDATE homestats t SET t.Street = %s, t.City = %s, t.Zip_Code = %s, t.Selling_Price = %s,
-    t.Listing_Price = %s, t.Sq_ft = %s,t.Rooms = %s,t.Baths = %s,t.Age_of_House = %s, WHERE t.id = %s """
+    inputData = (request.form.get('YourName'), request.form.get('YourSex'), request.form.get('YourAge'),
+                 request.form.get('YourHeight'), request.form.get('YourWeight'), biostat_id)
+    sql_update_query = """UPDATE biostats t SET t.YourName = %s, t.Sex = %s, t.Age = %s, t.Height_in = %s, t.Weight_lbs = %s WHERE t.id = %s """
     cursor.execute(sql_update_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
 
 
-@app.route('/homestats/new', methods=['GET'])
+@app.route('/biostats/new', methods=['GET'])
 def form_insert_get():
-    return render_template('new.html', title='New Homestat Form')
+    return render_template('new.html', title='New Biostat Form')
 
 
-@app.route('/homestats/new', methods=['POST'])
+@app.route('/biostats/new', methods=['POST'])
 def form_insert_post():
     cursor = mysql.get_db().cursor()
-    inputData = (request.form.get('Street'), request.form.get('City'), request.form.get('Zip_Code'),
-                 request.form.get('Selling_Price'), request.form.get('Listing_Price'),
-                 request.form.get('Listing_Price'), request.form.get('Sq_ft'), request.form.get('Rooms'),
-                 request.form.get('Baths'), request.form.get('Age_of_House'))
-    sql_insert_query = """INSERT INTO homestats (Street, City, Zip_Code, Selling_Price, Listing_Price, Sq_ft, Rooms, 
-    Baths, Age_of_House) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) """
+    inputData = (request.form.get('YourName'), request.form.get('Sex'), request.form.get('Age'),
+                 request.form.get('Height_in'), request.form.get('Weight_lbs'))
+    sql_insert_query = """INSERT INTO biostats (YourName,Sex,Age,Height_in,Weight_lbs) VALUES (%s, %s, %s, %s, %s) """
     cursor.execute(sql_insert_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
 
 
-@app.route('/delete/<int:homestat_id>', methods=['POST'])
-def form_delete_post(homestat_id):
+@app.route('/delete/<int:biostat_id>', methods=['POST'])
+def form_delete_post(biostat_id):
     cursor = mysql.get_db().cursor()
-    sql_delete_query = """DELETE FROM homestats WHERE id = %s """
-    cursor.execute(sql_delete_query, homestat_id)
+    sql_delete_query = """DELETE FROM biostats WHERE id = %s """
+    cursor.execute(sql_delete_query, biostat_id)
     mysql.get_db().commit()
     return redirect("/", code=302)
 
 
-@app.route('/api/v1/homestats', methods=['GET'])
+@app.route('/api/v1/biostats', methods=['GET'])
 def api_browse() -> str:
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM homestats')
+    cursor.execute('SELECT * FROM biostats')
     result = cursor.fetchall()
     json_result = json.dumps(result);
     resp = Response(json_result, status=200, mimetype='application/json')
     return resp
 
 
-@app.route('/api/v1/homestats/<int:homestat_id>', methods=['GET'])
-def api_retrieve(homestat_id) -> str:
+@app.route('/api/v1/biostats/<int:biostat_id>', methods=['GET'])
+def api_retrieve(biostat_id) -> str:
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM homestats WHERE id=%s', homestat_id)
+    cursor.execute('SELECT * FROM biostats WHERE id=%s', biostat_id)
     result = cursor.fetchall()
     json_result = json.dumps(result);
     resp = Response(json_result, status=200, mimetype='application/json')
     return resp
 
 
-@app.route('/api/v1/homestats/', methods=['POST'])
+@app.route('/api/v1/biostats/', methods=['POST'])
 def api_add() -> str:
     resp = Response(status=201, mimetype='application/json')
     return resp
 
 
-@app.route('/api/v1/homestats/<int:homestat_id>', methods=['PUT'])
-def api_edit(homestat_id) -> str:
+@app.route('/api/v1/biostats/<int:biostat_id>', methods=['PUT'])
+def api_edit(biostat_id) -> str:
     resp = Response(status=201, mimetype='application/json')
     return resp
 
 
-@app.route('/api/homestats/<int:homestat_id>', methods=['DELETE'])
-def api_delete(homestat_id) -> str:
+@app.route('/api/biostats/<int:biostat_id>', methods=['DELETE'])
+def api_delete(biostat_id) -> str:
     resp = Response(status=210, mimetype='application/json')
     return resp
 
